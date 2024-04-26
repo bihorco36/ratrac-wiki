627d983177748105c7f58d0bc3f92518ce032f94
Janiss Binder <binder@puzzle.ch>
2024-04-26 17:16:02 +0200

    Add main Structure and Design

---
+++ components/ultiplays.tsx
@@ -0,0 +1,7 @@
+// Example from https://beta.reactjs.org/learn
+
+export default function Ultiplays(props: any) {
+  return (
+      <iframe width="450px" height="300px" src={props.link} allowFullScreen></iframe>
+  )
+}
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,41 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>Ratrac Wiki</span>,
+  logo: (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div>
+        <img src={"/images/raettu_logo_rot.svg"} height={64} width={64} />
+      </div>
+      <div style={{ fontSize: "1.5em", marginTop: "5px", marginLeft: "10px" }}>
+        Ratrac Wiki
+      </div>
+    </div>
+  ),
   project: {
-    link: 'https://github.com/bihorco36/ratrac-wiki',
+    link: "https://github.com/bihorco36/ratrac-wiki",
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/bihorco36/ratrac-wiki/tree/main',
+  primaryHue: 360,
+  primarySaturation: { light: 90, dark: 60 },
+  docsRepositoryBase: "https://github.com/bihorco36/ratrac-wiki/tree/main",
   footer: {
-    text: 'Ratrac Wiki',
+    text: "Ratrac Wiki",
   },
-}
+  head: (
+    <>
+      <link
+        rel="shortcut icon"
+        type="image/x-icon"
+        href={"/images/raettu_logo_white.svg"}
+      />
+    </>
+  ),
+};
 
-export default config
+export default config;
